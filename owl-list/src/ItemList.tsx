5a7a51063610b4709eeeeac91cdeaa8de4e80bb4
Manu Nair <manunair3@gmail.com>
2020-12-16 14:30:47 -0800

    support virtualization using external library

--- owl-list/src/ItemList.tsx
+++ owl-list/src/ItemList.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { useVirtual } from "react-virtual";
 import Item from "./Item";
 
 import "./ItemList.css";
@@ -12,71 +13,102 @@ interface ItemListProps {
 const ItemList: React.FC<ItemListProps> = ({ items, onDeleteClick, swapItems }) => {
 
   const bottomRef = React.useRef<HTMLDivElement>(null);
-  const [showScrollToBottomButton, setShowScrollToBottomButton] = React.useState<boolean>(false);
   const [fromIndex, setFromIndex] = React.useState<number>(0);
   const [toIndex, setToIndex] = React.useState<number>(0);
 
-  React.useEffect(() => {
-    scrollToBottom();
-  }, [items.length, bottomRef]);
+  const VirtualizedItems = () => {
+    const [showScrollToBottomButton, setShowScrollToBottomButton] = React.useState<boolean>(false);
+    const parentRef = React.useRef<HTMLDivElement>(null);
 
-  const scrollToBottom = () => {
-    if (bottomRef !== null && bottomRef.current !== null) {
-      bottomRef.current.scrollIntoView({ behavior: "smooth" });
-    }
-  };
+    const rowVirtualizer = useVirtual({
+      size: items.length,
+      parentRef,
+      overscan: 5,
+    });
 
-  const observer = React.useRef(new IntersectionObserver(entries => {
-    const entry = entries[0];
+    const observer = React.useRef(new IntersectionObserver(entries => {
+      const entry = entries[0];
 
-    if (entry.isIntersecting) {
-      setShowScrollToBottomButton(false);
-    } else {
-      setShowScrollToBottomButton(true);
-    }
-  }, { threshold: 1 }));
+      if (entry.isIntersecting) {
+        setShowScrollToBottomButton(false);
+      } else {
+        setShowScrollToBottomButton(true);
+      }
+    }, { threshold: 1 }));
 
-  React.useEffect(() => {
-    const currentObserver = observer.current;
-    const currentRef = bottomRef.current;
-    if (currentRef) currentObserver.observe(currentRef);
+    React.useEffect(() => {
+      const currentObserver = observer.current;
+      const currentRef = bottomRef.current;
+      if (currentRef) currentObserver.observe(currentRef);
 
-    return () => {
-      if (currentRef) currentObserver.unobserve(currentRef);
+      return () => {
+        if (currentRef) currentObserver.unobserve(currentRef);
+      };
+    }, []);
+
+    // Ignore react-hooks/exhaustive-deps lint warning. we cannot add rowVirtualizer in to dependency array
+    // as it will result in infinite re-renders (unable to perform object comparison)
+    React.useEffect(() => {
+      // setTimeout to ensure scroll to bottom happens after all the items are rendered in case of large numbers
+      setTimeout(() => rowVirtualizer.scrollToIndex(items.length - 1, { align: 'start' }), 0);
+    }, [items.length]);
+
+    const onDragEnd = () => {
+      swapItems(fromIndex, toIndex);
+      // Once this PR (https://github.com/tannerlinsley/react-virtual/pull/55/files), we can recalculate the
+      // height of rows after they are reordered.
     };
-  }, []);
 
+    const onDragStart = (index: number) => {
+      setFromIndex(index);
+    };
 
+    const onDragEnter = (index: number) => {
+      setToIndex(index);
+    };
 
-  const onDragEnd = () => {
-    swapItems(fromIndex, toIndex);
-  };
-
-  const onDragStart = (index: number) => {
-    setFromIndex(index);
-  };
-
-  const onDragEnter = (index: number) => {
-    setToIndex(index);
-  };
+    return (
+      <div
+        ref={parentRef}
+        className="item-list-content">
+        <div
+          style={{
+            height: `${rowVirtualizer.totalSize}px`,
+            width: "100%",
+            position: "relative"
+          }}
+        >
+          {rowVirtualizer.virtualItems.map(virtualRow => (
+            <div
+              key={virtualRow.index}
+              ref={virtualRow.measureRef}
+              className="item"
+              style={{
+                height: `${items[virtualRow.index]}px`,
+                transform: `translateY(${virtualRow.start}px)`
+              }}
+            >
+              <Item
+                key={virtualRow.index}
+                text={items[virtualRow.index]}
+                isAlt={virtualRow.index % 2 === 0}
+                index={virtualRow.index}
+                onDeleteClick={onDeleteClick}
+                onDragEnd={onDragEnd}
+                onDragStart={onDragStart}
+                onDragEnter={onDragEnter} />
+            </div>
+          ))}
+          <div ref={bottomRef} className="bottom-ref-div"></div>
+        </div>
+        {showScrollToBottomButton && <button className="scroll-bottom-button" onClick={() => rowVirtualizer.scrollToIndex(items.length - 1)}>Scroll to bottom</button>}
+      </div>
+    );
+  }
 
   return (
     <div className="item-list-container">
-      <div className="item-list-content">
-        {items.map((item, index) => {
-          return <Item
-            key={index}
-            text={item}
-            isAlt={index % 2 === 0}
-            index={index}
-            onDeleteClick={onDeleteClick}
-            onDragEnd={onDragEnd}
-            onDragStart={onDragStart}
-            onDragEnter={onDragEnter} />
-        })}
-        <div ref={bottomRef}></div>
-      </div>
-      {showScrollToBottomButton && <button className="scroll-bottom-button" onClick={scrollToBottom}>Scroll to bottom</button>}
+      {VirtualizedItems()}
     </div>
   )
 };
